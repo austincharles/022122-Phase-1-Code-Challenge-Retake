b5f94dcbac083d383e22ba0bf55385d3ff48955f
Austin Holeman <austinholeman@gmail.com>
2022-03-10 09:47:40 -0800

    deliverable 1 complete

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-// Your code here
-console.log('hi')
+const URL = 'http://localhost:3000/films/1'
+const moviePoster = document.getElementById('poster')
+const movieTitle = document.getElementById('title')
+const movieShowtime = document.getElementById('showtime')
+const movieRuntime = document.getElementById('runtime')
+const movieDescription = document.getElementById('film-info')
+const ticketNum = document.getElementById('ticket-num')
+const convertedCapacity = parseInt(films.capacity, 10)
+
+fetch(URL)
+    .then(resp => resp.json())
+    .then(renderMovieDetails)
+
+function renderMovieDetails(data) {
+    moviePoster.src = data.poster
+    movieTitle.textContent = data.title
+    movieShowtime.textContent = data.showtime
+    movieRuntime.textContent = data.runtime
+    movieDescription.textContent = data.description
+    ticketNum.innerText = convertedCapacity - films.tickets_sold
+}
+
+const URL_2 = 'http://localhost:3000/films'
+
+fetch(URL_2)
+    .then(resp => resp.json())
+    .then(renderSideMenu)
+
+function renderSideMenu() {
+    
+}
